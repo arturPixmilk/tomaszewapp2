cf8337c9ccc643987ce77180ddbe0b828053eaf0
Artur Świątek <artur7pl@gmail.com>
2019-06-04 07:49:15 +0200

    fix

--- app/src/main/java/prm/tomaszewapp2/LoginActivity.kt
+++ app/src/main/java/prm/tomaszewapp2/LoginActivity.kt
@@ -10,9 +10,11 @@ import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
@@ -31,12 +33,19 @@ class LoginActivity : AppCompatActivity() {
 
         val loginButton = findViewById<Button>(R.id.loginButton)
         val registerButton = findViewById<Button>(R.id.registerButton)
-
+        val forgotPassword = findViewById<TextView>(R.id.forgotPasswordButton)
 
 
         loginButton.setOnClickListener {
             val login = findViewById<EditText>(R.id.loginInput).text.toString()
             val password = findViewById<EditText>(R.id.passwordInput).text.toString()
+            if (login.isNullOrEmpty() || password.isNullOrEmpty()) {
+                Toast.makeText(
+                    this, "Login and password cannot be empty.",
+                    Toast.LENGTH_SHORT
+                )
+                return@setOnClickListener
+            }
             auth.signInWithEmailAndPassword(login, password).addOnCompleteListener(this) {
                 if (it.isSuccessful) {
                     Log.d("AUTH", "signInWithEmail:success")
@@ -56,6 +65,13 @@ class LoginActivity : AppCompatActivity() {
         registerButton.setOnClickListener {
             val login = findViewById<EditText>(R.id.loginInput).text.toString()
             val password = findViewById<EditText>(R.id.passwordInput).text.toString()
+            if (login.isNullOrEmpty() || password.isNullOrEmpty()) {
+                Toast.makeText(
+                    this, "Login and password cannot be empty.",
+                    Toast.LENGTH_SHORT
+                )
+                return@setOnClickListener
+            }
             auth.createUserWithEmailAndPassword(login, password).addOnCompleteListener(this) {
                 if (it.isSuccessful) {
                     Log.d("AUTH", "createUserWithEmail:success")
@@ -70,6 +86,31 @@ class LoginActivity : AppCompatActivity() {
             }
         }
 
+        forgotPassword.setOnClickListener {
+            val login = findViewById<EditText>(R.id.loginInput).text.toString()
+            if (login.isNullOrEmpty()) {
+                Toast.makeText(
+                    this, "Login and password cannot be empty.",
+                    Toast.LENGTH_SHORT
+                )
+                return@setOnClickListener
+            }
+            auth.sendPasswordResetEmail(login).addOnCompleteListener(this) {
+                if (it.isSuccessful) {
+                    Log.d("AUTH", "sendPasswordResetEmail:success")
+                    Toast.makeText(
+                        this, "Password send to your email",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                } else {
+                    Log.d("AUTH", "sendPasswordResetEmail:failure")
+                    Toast.makeText(
+                        this, it.exception?.localizedMessage,
+                        Toast.LENGTH_SHORT
+                    )
+                }
+            }
+        }
     }
 
 }
